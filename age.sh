215976e210e5e83e89e356ec7a79e294675ca596
ramkumar <ram@gmail.com>
2024-10-01 15:38:19 +0530

    age modified

--- age.sh
+++ age.sh
@@ -6,9 +6,12 @@ echo "child"
 elif [ $age -ge 13 -a $age -le 19 ]
 then
 echo "teenager"
-elif [ $age -gt 20 ]
+elif [ $age -gt 20 -a $age -le 59 ]
 then
 echo "adult"
+elif [ $age -gt 60 ]
+then
+echo "oldage"
 else
 echo "nil"
 fi
